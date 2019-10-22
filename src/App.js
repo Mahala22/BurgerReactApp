7b9695c5c7a441dd0f512e101b0b92b857d2a096
Mahala22 <adarshmahala@gmail.com>
2019-10-22 23:56:04 +0530

    Burger App Hoc implementation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, {Component} from 'react';
 import './App.css';
-
+import Layout from './components/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 class App extends Component{
-  
-  
- render(){
-  return (
-    <div>
-      <h1>Hello lets begin the burger App</h1>
+
+
+  render() {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder/>
+        </Layout>
       </div>
-  );
- }
+    );
+  }
 }
 
 
