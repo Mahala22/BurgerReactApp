094d08ef416f7dadea8e5ed2e52a53a02515e8ee
Mahala22 <adarshmahala@gmail.com>
2020-01-29 15:46:53 +0530

    NAvigation , logo and sidedrawer implementation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,9 +2,11 @@ import React from 'react'
 import Aux from '../../hoc/Auxiliary'
 import classes from '../Layout/Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
 const layout = (props) => (
     <Aux>
         <Toolbar/>
+        <SideDrawer/>
         <main className={classes.Content}>
             {props.children}
         </main>
